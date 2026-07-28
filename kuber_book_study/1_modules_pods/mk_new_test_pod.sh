dcef46e6c8432e2e9394214d9b712c99ae6e476e
shoelacevip12 <shoelacevip21@gmail.com>
2026-07-28 21:44:44 +0300

    some_word_add_module_1

--- kuber_book_study/1_modules_pods/mk_new_test_pod.sh
+++ kuber_book_study/1_modules_pods/mk_new_test_pod.sh
@@ -3,14 +3,21 @@
 mkdir -vp /home/namespace/box/{bin,lib,lib64,proc}
 
 cp -v /bin/{ls,bash} /home/namespace/box/bin
-cp -v /usr/bin/{kill,ps} /home/namespace/box/bin
+cp -v /usr/bin/{kill,ps,ip} /home/namespace/box/bin
+cp -vr /usr/sbin/ip /home/namespace/box/bin
+cp -vr /usr/bin/curl /home/namespace/box/bin
 
 cp -r /lib/* /home/namespace/box/lib
 cp -vr /lib64/* /home/namespace/box/lib64
+
+
 
 mount -t proc /proc /home/namespace/box/proc
 
 # для изоляции процессов и файловых дескрипторов в chroot окружении
 # unshare -p -f --mount-proc=home/namespace/box/proc
+
+# для изоляции сетевого пространства имен в chroot окружении
+# unshare -p -n -f --mount-proc=/home/namespace/box/proc
 
 chroot /home/namespace/box /bin/bash
