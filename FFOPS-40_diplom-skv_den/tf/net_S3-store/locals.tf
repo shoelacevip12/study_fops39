6567c299be0b25e75a8ba50979c65a08de3ee624
shoelacevip12 <shoelacevip21@gmail.com>
2026-09-19 19:03:22 +0300

    commit10, FFOPS-40_diplom-skv_den

--- FFOPS-40_diplom-skv_den/tf/net_S3-store/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  subnet_array = flatten([for k, v in var.subnets : [for j in v : {
-    name = j.name
-    zone = j.zone
-    cidr = j.cidr
-    }
-  ]])
-}
--- FFOPS-40_diplom-skv_den/tf/net_S3-store/sleep_timer.tf
+++
@@ -1,9 +0,0 @@
-resource "time_sleep" "iam_propagation" {
-  /*
-    задержка после создания IAM-биндинга sa_encrypterDecrypter, 
-    чтобы провайдер успел прочитать его обратно
-    из-за ручного import ресурсов terraform
-    */
-  depends_on      = [yandex_resourcemanager_folder_iam_member.sa_encrypterDecrypter]
-  create_duration = "30s"
-}
